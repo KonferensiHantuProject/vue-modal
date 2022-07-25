068953b476e986975317d193d17c0d45bbd07447
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-07-25 08:40:53 +0700

    First Lesson Video Part One

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import the modal from component
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Modal
+  },
+  data () {
+    return {
+      title: 'Aplikasi Vue Pertama'
+    }
   }
 }
 </script>
@@ -22,5 +28,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<!-- HTML -->
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>Modal Konten</p>
+        </div>
+    </div>
+</template>
+
+<!-- Style -->
+<style>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
